25e6208725c871788b69e002f08164075e950567
Ilya Vlasov <ilja.vlasov2012@yandex.ru>
2020-05-18 01:34:50 +0300

    Changed structure, added autos

---
+++ src/Controller/Pages/Staff/StaffDeliveriesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\StatusHistory;
+use App\Entity\Warehouse;
+use App\Form\AddDeliveryStatusFormType;
+use App\Service\DeliveryOperationService;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class StaffDeliveriesController extends AbstractController
+{
+
+    public function displayPage(Request $request)
+    {
+        return $this->render('pages/staff_deliveries.html.twig');
+    }
+}
